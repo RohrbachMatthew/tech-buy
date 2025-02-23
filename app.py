849d987f7aeac4366df06f741759fe0e0aeb34ab
noxian-byte <amaliecs99@gmail.com>
2025-02-23 14:32:05 -0500

    setup the frontend

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Home Page
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("index.html")
 
+# Products Page
+@app.route("/products")
+def products():
+    return render_template("products.html")
+
+# Login Page
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
